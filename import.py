4a090df49ff01cdc1dfa6c66bb80998d155bfcea
Todd Trimble <trimble@sep.com>
2020-04-04 15:51:16 -0400

    Refactor to functions

--- import.py
+++ import.py
@@ -1,14 +1,19 @@
 import pandas as pandas
 import datetime
 
-url = "https://github.com/CSSEGISandData/COVID-19/raw/web-data/data/cases.csv"
+def get_data_for_date(targetDate, state):
+  # url = "https://github.com/CSSEGISandData/COVID-19/raw/web-data/data/cases.csv"
+  url = f"https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_daily_reports/{targetDate}.csv"
+  df = pandas.read_csv(url)[['Province_State', 'Admin2', 'Confirmed', 'Deaths']]
+  return df[df.Province_State.eq(state)]
 
-data = pandas.read_csv(url)[['Province_State', 'Admin2', 'Confirmed', 'Deaths']]
+def template_table(df, targetDate):
+  data = df
+  totals = data.sum()
+  print(f"{{| class=\"wikitable sortable\" style=\"text-align:right\"\n|+Coronavirus disease 2019 (COVID-19) cases in Indiana<ref>{{{{Cite web|url=https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_daily_reports/{targetDate}.csv|title=2019 Novel Coronavirus COVID-19 (2019-nCoV) Data Repository by Johns Hopkins CSSE|access-date={datetime.datetime.today().strftime('%Y-%m-%d')}}}}}</ref>\n! County || Confirmed Cases || Deaths")
+  for index, row in df.iterrows():
+    print(f"|-\n|style=\"text-align:left;\"|[[{row['Admin2']} County, Indiana|{row['Admin2']}]]||{row['Confirmed']}||{row['Deaths']}")
+  print(f"|-\n! style=\"text-align:right;\" |Total\n! style=\"text-align:right;\" |{totals.Confirmed}\n! style=\"text-align:right;\" |{totals.Deaths}\n|}}")
 
-indiana = data[data.Province_State.eq('Indiana')]
-totals = indiana.sum()
-
-print(f"{{| class=\"wikitable sortable\" style=\"text-align:right\"\n|+Coronavirus disease 2019 (COVID-19) cases in Indiana<ref>{{{{Cite web|url={url}|title=2019 Novel Coronavirus COVID-19 (2019-nCoV) Data Repository by Johns Hopkins CSSE|access-date={datetime.datetime.today().strftime('%Y-%m-%d')}}}}}</ref>\n! County || Confirmed Cases || Deaths")
-for index, row in indiana.iterrows():
-  print(f"|-\n|style=\"text-align:left;\"|[[{row['Admin2']} County, Indiana|{row['Admin2']}]]||{row['Confirmed']}||{row['Deaths']}")
-print(f"|-\n! style=\"text-align:right;\" |Total\n! style=\"text-align:right;\" |{totals.Confirmed}\n! style=\"text-align:right;\" |{totals.Deaths}\n|}}")
+df = get_data_for_date('04-02-2020', 'Indiana')
+template_table(df,  '04-02-2020')
