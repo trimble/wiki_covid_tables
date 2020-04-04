325c83af67ad7c8b4072d88ee441339198b492e6
Todd Trimble <trimble@sep.com>
2020-04-04 16:25:20 -0400

    Rename badly-named column

--- import.py
+++ import.py
@@ -4,7 +4,7 @@ import datetime
 def get_data_for_date(targetDate, state):
   # url = "https://github.com/CSSEGISandData/COVID-19/raw/web-data/data/cases.csv"
   url = f"https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_daily_reports/{targetDate}.csv"
-  df = pandas.read_csv(url)[['Province_State', 'Admin2', 'Confirmed', 'Deaths']]
+  df = pandas.read_csv(url)[['Province_State', 'Admin2', 'Confirmed', 'Deaths']].rename(columns={"Admin2": "County"})
   return df[df.Province_State.eq(state)]
 
 def template_table(df, targetDate):
@@ -12,7 +12,7 @@ def template_table(df, targetDate):
   totals = data.sum()
   print(f"{{| class=\"wikitable sortable\" style=\"text-align:right\"\n|+Coronavirus disease 2019 (COVID-19) cases in Indiana<ref>{{{{Cite web|url=https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_daily_reports/{targetDate}.csv|title=2019 Novel Coronavirus COVID-19 (2019-nCoV) Data Repository by Johns Hopkins CSSE|access-date={datetime.datetime.today().strftime('%Y-%m-%d')}}}}}</ref>\n! County || Confirmed Cases || Deaths")
   for index, row in df.iterrows():
-    print(f"|-\n|style=\"text-align:left;\"|[[{row['Admin2']} County, Indiana|{row['Admin2']}]]||{row['Confirmed']}||{row['Deaths']}")
+    print(f"|-\n|style=\"text-align:left;\"|[[{row['County']} County, Indiana|{row['County']}]]||{row['Confirmed']}||{row['Deaths']}")
   print(f"|-\n! style=\"text-align:right;\" |Total\n! style=\"text-align:right;\" |{totals.Confirmed}\n! style=\"text-align:right;\" |{totals.Deaths}\n|}}")
 
 df = get_data_for_date('04-02-2020', 'Indiana')
