b2f04c4b30b66db4d05f5c7eb1c8e42406e0277d
Todd Trimble <trimble@sep.com>
2020-04-05 10:27:56 -0400

    Rename county table function

--- import.py
+++ import.py
@@ -8,7 +8,7 @@ def get_data_for_date(targetDate):
   return pandas.read_csv(url)[['Province_State', 'Admin2', 'Confirmed', 'Deaths']].rename(columns={"Admin2": "County"})
 
 
-def county_table(df, targetDate, state):
+def generate_county_table(df, targetDate, state):
   data = df[df.Province_State.eq(state)]
   totals = data.sum()
   print(f"{{| class=\"wikitable sortable\" style=\"text-align:right\"\n|+Coronavirus disease 2019 (COVID-19) cases in {state}<ref>{{{{Cite web|url=https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_daily_reports/{targetDate}.csv|title=2019 Novel Coronavirus COVID-19 (2019-nCoV) Data Repository by Johns Hopkins CSSE|access-date={datetime.datetime.today().strftime('%Y-%m-%d')}}}}}</ref>\n! County || Confirmed Cases || Deaths")
@@ -25,7 +25,7 @@ if args.county_table:
   date = "04-04-2020"
   df = get_data_for_date(date)
   totals = df.sum()
-  county_table(df, date, args.state)
+  generate_county_table(df, date, args.state)
 else:
   for i in range(1, 5):
     date = f"04-{i:02d}-2020"
