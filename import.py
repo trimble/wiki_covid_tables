92a2b7c75d4b0d0a0096d23590a3658da122dca4
Todd Trimble <trimble@sep.com>
2020-04-21 19:13:24 -0400

    Update to use new ISDH data source

--- import.py
+++ import.py
@@ -3,23 +3,31 @@ import datetime
 import pandas as pd
 
 def get_data():
-  url = "https://coronavirus.in.gov/map-test/covid_report_county.csv"
-  return pd.read_csv(url)[['COUNTY_NAME','COVID_COUNT','COVID_DEATHS']]
+  url = f"https://hub.mph.in.gov/dataset/89cfa2e3-3319-4d31-a60d-710f76856588/resource/8b8e6cd7-ede2-4c41-a9bd-4266df783145/download/corona"
+  return pd.read_excel(url)[['COUNTY_NAME','COVID_COUNT','COVID_DEATHS','COVID_TEST']]
 
+# def get_data_from_api():
+#   import io
+#   import urllib
+#   url = 'https://hub.mph.in.gov/api/3/action/datastore_search?resource_id=8b8e6cd7-ede2-4c41-a9bd-4266df783145'
+#   fileobj = urllib.urlopen(url)
+#   print io.read()
 
 def generate_county_table(data, state):
   totals = data.sum()
-  print(f"{{| class=\"wikitable sortable\" style=\"text-align:right\"\n|+Coronavirus disease 2019 (COVID-19) cases in {state}<ref>{{{{Cite web|url=https://coronavirus.in.gov/map-test/covid_report_county.csv|title=ISDH - Novel Coronavirus: Novel Coronavirus (COVID-19)|access-date={datetime.datetime.today().strftime('%Y-%m-%d')}}}}}</ref>\n! County || Confirmed Cases || Deaths")
+  print("==Statistics==")
+  print(f"{{| class=\"wikitable sortable\" style=\"text-align:right\"\n|+Coronavirus disease 2019 (COVID-19) cases in {state}<ref>{{{{Cite web|url=https://hub.mph.in.gov/dataset/89cfa2e3-3319-4d31-a60d-710f76856588/resource/8b8e6cd7-ede2-4c41-a9bd-4266df783145/download/corona|title=ISDH - Novel Coronavirus: Novel Coronavirus (COVID-19)|access-date={datetime.datetime.today().strftime('%Y-%m-%d')}}}}}</ref>\n! County || Confirmed Cases || Deaths")
   for index, row in data.iterrows():
-    county_name = row['COUNTY_NAME'].title().replace("Dekalb", "DeKalb").replace("Laporte", "LaPorte").replace("Lagrange", "LaGrange")
-    print(f"|-\n|style=\"text-align:left;\"|[[{county_name} County, {state}|{county_name}]]||{row['COVID_COUNT']}||{row['COVID_DEATHS']}")
-  print(f"|-\n! style=\"text-align:right;\" |Total\n! style=\"text-align:right;\" |{totals.COVID_COUNT}\n! style=\"text-align:right;\" |{totals.COVID_DEATHS}\n|}}")
+    county_name = row['COUNTY_NAME'].title().replace("Dekalb", "DeKalb").replace("Laporte", "LaPorte").replace("Lagrange", "LaGrange").replace("St Joseph", "St. Joseph")
+    print(f"|-\n|style=\"text-align:left;\"|[[{county_name} County, {state}|{county_name}]]||{row['COVID_COUNT']:,}||{row['COVID_DEATHS']:,}")
+  print(f"|-\n! style=\"text-align:right;\" |Total\n! style=\"text-align:right;\" |{totals.COVID_COUNT:,}\n! style=\"text-align:right;\" |{totals.COVID_DEATHS:,}\n|}}")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--county_table', help='generate county-by-county infection table in wikimedia format', action='store_true')
 args = parser.parse_args()
 
 if args.county_table:
+  #df = get_data_from_api()
   df = get_data()
   generate_county_table(df, 'Indiana')
 else:
@@ -31,4 +39,4 @@ else:
   for index, row in df.iterrows():
     row['deaths_change'] = f"{row['deaths_change']:.0%}" if row['deaths_change'] else ''
     row['cases_change'] = f"{row['cases_change']:.0%}" if row['cases_change'] else ''
-    print(f"{row['date']};{row['deaths']};;{row['cases']};;;{row['cases']};{row['cases_change']};{row['deaths']};{row['deaths_change']}")
+    print(f"{row['date']};{row['deaths']:,};;{row['cases']:,};;;{row['cases']:,};{row['cases_change']};{row['deaths']:,};{row['deaths_change']}")
