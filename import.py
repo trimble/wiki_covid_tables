d3c663e2eb0a78c5338a1407fa0e95522963d2ad
Todd Trimble <trimble@sep.com>
2020-04-25 16:48:43 -0400

    Update to match format Cerebral726 uses
    * In order to avoid duplicate/weird/conflicting changes in the future,
      add the extra space and remove the commas in spots to match
      u|Cerebral726's preferred format.

--- import.py
+++ import.py
@@ -13,14 +13,14 @@ def get_data():
 #   fileobj = urllib.urlopen(url)
 #   print io.read()
 
-def generate_county_table(data, state):
+def generate_county_table(data):
   totals = data.sum()
   print("==Statistics==")
-  print(f"{{| class=\"wikitable sortable\" style=\"text-align:right\"\n|+Coronavirus disease 2019 (COVID-19) cases in {state}<ref>{{{{Cite web|url=https://hub.mph.in.gov/dataset/89cfa2e3-3319-4d31-a60d-710f76856588/resource/8b8e6cd7-ede2-4c41-a9bd-4266df783145/download/corona|title=ISDH - Novel Coronavirus: Novel Coronavirus (COVID-19)|access-date={datetime.datetime.today().strftime('%Y-%m-%d')}}}}}</ref>\n! County || Confirmed Cases || Deaths")
+  print(f"{{| class=\"wikitable sortable\" style=\"text-align:right\"\n|+Coronavirus disease 2019 (COVID-19) cases in Indiana Counties<ref>{{{{Cite web|url=https://hub.mph.in.gov/dataset/89cfa2e3-3319-4d31-a60d-710f76856588/resource/8b8e6cd7-ede2-4c41-a9bd-4266df783145/download/corona|title=ISDH - Novel Coronavirus: Novel Coronavirus (COVID-19)|access-date={datetime.datetime.today().strftime('%Y-%m-%d')}}}}}</ref>\n! County || Confirmed Cases || Deaths")
   for index, row in data.iterrows():
     county_name = row['COUNTY_NAME'].title().replace("Dekalb", "DeKalb").replace("Laporte", "LaPorte").replace("Lagrange", "LaGrange").replace("St Joseph", "St. Joseph")
-    print(f"|-\n|style=\"text-align:left;\"|[[{county_name} County, {state}|{county_name}]]||{row['COVID_COUNT']:,}||{row['COVID_DEATHS']:,}")
-  print(f"|-\n! style=\"text-align:right;\" |Total\n! style=\"text-align:right;\" |{totals.COVID_COUNT:,}\n! style=\"text-align:right;\" |{totals.COVID_DEATHS:,}\n|}}")
+    print(f"|- \n|style=\"text-align:left;\"|[[{county_name} County, Indiana|{county_name}]]||{row['COVID_COUNT']:,}||{row['COVID_DEATHS']:,}")
+  print(f"|- \n! style=\"text-align:right;\" |Total\n! style=\"text-align:right;\" |{totals.COVID_COUNT:,}\n! style=\"text-align:right;\" |{totals.COVID_DEATHS:,}\n|}}")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--county_table', help='generate county-by-county infection table in wikimedia format', action='store_true')
@@ -29,7 +29,7 @@ args = parser.parse_args()
 if args.county_table:
   #df = get_data_from_api()
   df = get_data()
-  generate_county_table(df, 'Indiana')
+  generate_county_table(df)
 else:
   df = pd.read_csv('incovid.csv')
   df = df.assign(cases_change=df.cases.pct_change())
@@ -39,4 +39,4 @@ else:
   for index, row in df.iterrows():
     row['deaths_change'] = f"{row['deaths_change']:.0%}" if row['deaths_change'] else ''
     row['cases_change'] = f"{row['cases_change']:.0%}" if row['cases_change'] else ''
-    print(f"{row['date']};{row['deaths']:,};;{row['cases']:,};;;{row['cases']:,};{row['cases_change']};{row['deaths']:,};{row['deaths_change']}")
+    print(f"{row['date']};{row['deaths']};;{row['cases']};;;{row['cases']};{row['cases_change']};{row['deaths']};{row['deaths_change']}")
