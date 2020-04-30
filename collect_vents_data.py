3d16355eb1bbe7b9824ea16441def23080cb60d4
Todd Trimble <trimble@sep.com>
2020-04-29 22:46:36 -0400

    Fix script that collects beds/vents data

--- collect_vents_data.py
+++ collect_vents_data.py
@@ -1,22 +1,16 @@
-# import argparse
 import datetime
 import pandas as pd
 
 def get_todays_beds_and_vents_data():
   url = f"https://hub.mph.in.gov/dataset/5a905d51-eb50-4a83-8f79-005239bd108b/resource/882a7426-886f-48cc-bbe0-a8d14e3012e4/download/covid_report_bedvent.xlsx"
-  return pd.read_excel(url)
-
-def get_compiled_data():
-  file = f"covid_report_bedvent_compiled.xlsx"
-  return pd.read_excel(file, index_col='date')
+  return pd.read_excel(url, index_col='STATUS_TYPE')
 
 if __name__ == "__main__":
-  # parser = argparse.ArgumentParser()
-  # parser.add_argument('-c', '--county_table', help='generate county-by-county infection table in wikimedia format', action='store_true')
-  # args = parser.parse_args()
-
-  new_data = get_todays_beds_and_vents_data().rename(columns={'TOTAL':f"{datetime.date.today()}", 'STATUS_TYPE':'date'}).set_index('date').transpose()
-  old_data = get_compiled_data()
-  data = pd.concat([old_data,new_data])
-  print(data)
-  data.to_excel('covid_report_bedvent_compiledxxx.xlsx')
+  file = f"covid_report_bedvent_compiled.csv"
+  new_data = get_todays_beds_and_vents_data()
+  new_row = new_data['TOTAL']
+  old_data = pd.read_csv(file)
+  old_data[f"{datetime.date.today()}"] = old_data['STATUS_TYPE'].map(new_row)
+  old_data.set_index('STATUS_TYPE', inplace=True)
+  print(old_data)
+  old_data.to_csv(file)
