0adc489a00a85666b9b9bb351cace7e0863bb9d4
Todd Trimble <trimble@sep.com>
2020-05-03 15:38:56 -0400

    Use more explicit county population defintion

--- import.py
+++ import.py
@@ -4,11 +4,109 @@ import pandas as pd
 
 def get_county_data():
   url = f"https://hub.mph.in.gov/dataset/89cfa2e3-3319-4d31-a60d-710f76856588/resource/8b8e6cd7-ede2-4c41-a9bd-4266df783145/download/covid_report_county.xlsx"
-  return pd.read_excel(url)[['COUNTY_NAME','COVID_COUNT','COVID_DEATHS','COVID_TEST']]
+  isdh_data = pd.read_excel(url)[['COUNTY_NAME','COVID_COUNT','COVID_DEATHS','COVID_TEST']]
+
+  populations=pd.DataFrame([
+    ("ADAMS", 35777),
+    ("ALLEN", 379299),
+    ("BARTHOLOMEW", 83779),
+    ("BENTON", 8748),
+    ("BLACKFORD", 11758),
+    ("BOONE", 67843),
+    ("BROWN", 15092),
+    ("CARROLL", 20257),
+    ("CASS", 37689),
+    ("CLARK", 118302),
+    ("CLAY", 26225),
+    ("CLINTON", 32399),
+    ("CRAWFORD", 10577),
+    ("DAVIESS", 33351),
+    ("DEARBORN", 49458),
+    ("DECATUR", 26559),
+    ("DEKALB", 43475),
+    ("DELAWARE", 114135),
+    ("DUBOIS", 42736),
+    ("ELKHART", 206341),
+    ("FAYETTE", 23102),
+    ("FLOYD", 78522),
+    ("FOUNTAIN", 16346),
+    ("FRANKLIN", 22758),
+    ("FULTON", 19974),
+    ("GIBSON", 33659),
+    ("GRANT", 65769),
+    ("GREENE", 31922),
+    ("HAMILTON", 338011),
+    ("HANCOCK", 78168),
+    ("HARRISON", 40515),
+    ("HENDRICKS", 170311),
+    ("HENRY", 47972),
+    ("HOWARD", 82544),
+    ("HUNTINGTON", 36520),
+    ("JACKSON", 44231),
+    ("JASPER", 33562),
+    ("JAY", 20436),
+    ("JEFFERSON", 32308),
+    ("JENNINGS", 27735),
+    ("JOHNSON", 158167),
+    ("KNOX", 36594),
+    ("KOSCIUSKO", 79456),
+    ("LAGRANGE", 39614),
+    ("LAKE", 485493),
+    ("LAPORTE", 109888),
+    ("LAWRENCE", 45370),
+    ("MADISON", 129569),
+    ("MARION", 964582),
+    ("MARSHALL", 46258),
+    ("MARTIN", 10255),
+    ("MIAMI", 35516),
+    ("MONROE", 148431),
+    ("MONTGOMERY", 38338),
+    ("MORGAN", 70489),
+    ("NEWTON", 13984),
+    ("NOBLE", 47744),
+    ("OHIO", 5875),
+    ("ORANGE", 19646),
+    ("OWEN", 20799),
+    ("PARKE", 16937),
+    ("PERRY", 19169),
+    ("PIKE", 12389),
+    ("PORTER", 170389),
+    ("POSEY", 25427),
+    ("PULASKI", 12353),
+    ("PUTNAM", 37576),
+    ("RANDOLPH", 24665),
+    ("RIPLEY", 28324),
+    ("RUSH", 16581),
+    ("ST JOSEPH", 271826),
+    ("SCOTT", 23873),
+    ("SHELBY", 44729),
+    ("SPENCER", 20277),
+    ("STARKE", 22995),
+    ("STEUBEN", 34594),
+    ("SULLIVAN", 20669),
+    ("SWITZERLAND", 10751),
+    ("TIPPECANOE", 195732),
+    ("TIPTON", 15148),
+    ("UNION", 7054),
+    ("VANDERBURGH", 181451),
+    ("VERMILLION", 15498),
+    ("VIGO", 107038),
+    ("WABASH", 30996),
+    ("WARREN", 8265),
+    ("WARRICK", 62998),
+    ("WASHINGTON", 28036),
+    ("WAYNE", 65884),
+    ("WELLS", 28296),
+    ("WHITE", 24102),
+    ("WHITLEY", 33964)], columns=['COUNTY_NAME', 'population'])
+  populations.set_index('COUNTY_NAME', inplace=True)
+  isdh_data.set_index('COUNTY_NAME', inplace=True)
+  isdh_data = pd.concat([isdh_data,populations], axis=1)
+  return(isdh_data)
 
 def get_beds_and_vents_data():
   url = f"https://hub.mph.in.gov/dataset/5a905d51-eb50-4a83-8f79-005239bd108b/resource/882a7426-886f-48cc-bbe0-a8d14e3012e4/download/covid_report_bedvent.xlsx"
-  return pd.read_excel(url, index_col='STATUS_TYPE')
+  data = pd.read_excel(url, index_col='STATUS_TYPE')
 
 def get_trend_data():
   url = f"https://hub.mph.in.gov/dataset/ab9d97ab-84e3-4c19-97f8-af045ee51882/resource/182b6742-edac-442d-8eeb-62f96b17773e/download/covid-19_statewidetestcasedeathtrends_428.xlsx"
@@ -19,10 +117,9 @@ def generate_county_table(data):
 
   list = []
   for index, row in data.iterrows():
-    county_name = row['COUNTY_NAME'].title().replace("Dekalb", "DeKalb").replace("Laporte", "LaPorte").replace("Lagrange", "LaGrange").replace("St Joseph", "St. Joseph")
+    county_name = index.title().replace("Dekalb", "DeKalb").replace("Laporte", "LaPorte").replace("Lagrange", "LaGrange").replace("St Joseph", "St. Joseph")
     list.append("|-")
     list.append(f"! style=\"padding:0px 2px;\" |[[{county_name} County, Indiana|{county_name}]]")
-    # print(f"|- \n|style=\"text-align:left;\"|[[{county_name} County, Indiana|{county_name}]]||{row['COVID_COUNT']:,}||{row['COVID_DEATHS']:,}")
     list.append(f"| style=\"padding:0px 2px;\" |{row['COVID_COUNT']:,}")
     list.append(f"| style=\"padding:0px 2px;\" |{row['COVID_DEATHS']:,}")
     list.append(f"| style=\"padding:0px 2px;\" |{{{{–}}}}")
@@ -31,7 +128,6 @@ def generate_county_table(data):
     list.append(f"| style=\"padding:0px 2px;\" |")
 
   separator = '\n'
-
 
   table_template = f"""<div class="tp-container" style="float:left;max-width:100%;overflow-y:auto;padding-right:0em;margin: 0 0 0.5em 1em">
 {{| class="wikitable plainrowheaders sortable" style="text-align:right; font-size:85%; margin:0"
@@ -143,98 +239,6 @@ if __name__ == "__main__":
 
   if args.county_table:
     trend = get_county_data()
-    trend = trend.assign(population=[35777,
-      379299,
-      83779,
-      8748,
-      11758,
-      67843,
-      15092,
-      20257,
-      37689,
-      118302,
-      26225,
-      32399,
-      10577,
-      33351,
-      49458,
-      26559,
-      43475,
-      114135,
-      42736,
-      206341,
-      23102,
-      78522,
-      16346,
-      22758,
-      19974,
-      33659,
-      65769,
-      31922,
-      338011,
-      78168,
-      40515,
-      170311,
-      47972,
-      82544,
-      36520,
-      44231,
-      33562,
-      20436,
-      32308,
-      27735,
-      158167,
-      36594,
-      79456,
-      39614,
-      485493,
-      109888,
-      45370,
-      129569,
-      964582,
-      46258,
-      10255,
-      35516,
-      148431,
-      38338,
-      70489,
-      13984,
-      47744,
-      5875,
-      19646,
-      20799,
-      16937,
-      19169,
-      12389,
-      170389,
-      25427,
-      12353,
-      37576,
-      24665,
-      28324,
-      16581,
-      23873,
-      44729,
-      20277,
-      271826,
-      22995,
-      34594,
-      20669,
-      10751,
-      195732,
-      15148,
-      7054,
-      181451,
-      15498,
-      107038,
-      30996,
-      8265,
-      62998,
-      28036,
-      65884,
-      28296,
-      24102,
-      33964])
     generate_county_table(trend)
   elif args.info_box:
     beds_and_vents = get_beds_and_vents_data()
